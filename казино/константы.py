8adf540e5810f51eed5ac237117cd890ffa0fd51
Roman Kosarev <rmksrv@outlook.com>
2024-12-21 22:02:52 +0400

    компиляция первой программы

---
+++ казино/константы.py
@@ -0,0 +1,13 @@
+ПИТОН_ОПКОДЫ = {
+    "RESUME": 151,
+    "POP_TOP": 1,
+    "PUSH_NULL": 2,
+    "BINARY_OP": 122,
+    "RETURN_CONST": 121,
+    "STORE_NAME": 90,
+    "LOAD_CONST": 100,
+    "LOAD_NAME": 101,
+    "CALL": 171,
+}
+# МАГИЧЕСКОЕ_ЧИСЛО_ПИТОНА = 0xcb0d0d0a
+МАГИЧЕСКОЕ_ЧИСЛО_ПИТОНА = b"\xcb\r\r\n\x00\x00\x00\x00"
--- казино/типы.py
+++ казино/типы.py
@@ -1,13 +1,13 @@
 import typing as т
 
-type Кортеж = tuple
+type ПроизвКортеж[Т] = tuple[Т, ...]
 type Список[T] = list[T]
 type Словарь[K, V] = dict[K, V]
-type Стэк = Список[int]
 type СтдТипы = str | int | float | bool | None
 type Вызывабельное = т.Callable
-type ТаблицаИмен = Словарь[int, Кортеж[str, СтдТипы | Вызывабельное]]
-type КонстПул = Кортеж[СтдТипы, ...]
+type Стэк = Список[СтдТипы | Вызывабельное]
+type ТаблицаИмен = Словарь[int, tuple[str, СтдТипы | Вызывабельное]]
+type КонстПул = ПроизвКортеж[СтдТипы]
 
 Ничего = None
 Мб = т.Optional
